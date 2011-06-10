a5173e6593891d01e70fab36e14cb15e436d5801
James Hall <rio@x5g.com>
2011-06-10 17:56:48 +0100

    Print out Internal Speakers or Headphones

--- headphone-detector/main.m
+++ headphone-detector/main.m
@@ -7,15 +7,33 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <CoreAudio/CoreAudio.h>
 
 int main (int argc, const char * argv[])
 {
-
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-
-    // insert code here...
-    NSLog(@"Hello, World!");
-
+    
+    AudioDeviceID deviceID;
+    UInt32 size = sizeof(deviceID);
+    OSStatus err = AudioHardwareGetProperty(kAudioHardwarePropertyDefaultSystemOutputDevice, &size, &deviceID);
+    NSCAssert((err == noErr), @"AudioHardwareGetProperty failed to get the kAudioHardwarePropertyDefaultSystemOutputDevice property");
+    //To be notified when something is plugged/unplugged into the headphones jack
+    //listen for a kAudioDevicePropertyDataSource or kAudioDevicePropertyDataSources notification on deviceID
+    
+    //Check if headphones are plugged in right now:
+    UInt32 dataSource;
+    size = sizeof(dataSource);
+    err = AudioDeviceGetProperty(deviceID, 0, 0, kAudioDevicePropertyDataSource, &size, &dataSource);
+    NSCAssert((err == noErr), @"AudioDeviceGetProperty failed to get the kAudioDevicePropertyDataSource property");
+    
+    //'ispk' => internal speakers
+    //'hdpn' => headphones
+    if(dataSource == 'ispk') {
+        printf("Internal Speakers");
+    } else {
+        printf("Headphones");
+    }
+    
     [pool drain];
     return 0;
 }
