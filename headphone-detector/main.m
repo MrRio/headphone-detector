fc1ac976ae6ef7ae1a39106ddccd80a5ea00748a
James Hall <rio@x5g.com>
2011-06-10 17:44:03 +0100

    Initial Commit

---
+++ headphone-detector/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  headphone-detector
+//
+//  Created by James Hall on 10/06/2011.
+//  Copyright 2011 Snapshot Media. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
